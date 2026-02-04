78dee6c66fe2039ed9d3f0266a3ee30c30c37eff
1dawd23 <1dawd23@A204PC25.desarrolloweb.local>
2026-02-04 10:24:33 +0100

    Tienda completa, con alguna falla como el idCliente

---
+++ src/main/java/cm69ikereducastur/com/tienda2026clase/StockCero.java
@@ -0,0 +1,16 @@
+package cm69ikereducastur.com.tienda2026clase;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 1dawd23
+ */
+public class StockCero extends Exception{
+    public StockCero(String mensaje){
+        super(mensaje);
+    }
+}
